08e51f0de62485018a68ff4f8af5cb84a70069b1
Michael Brunco <brunco.michael@gmail.com>
2026-07-06 10:30:04 +0200

    v1 ng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Services } from './pages/services/services';
+import { Gallery } from './pages/gallery/gallery';
+import { Reviews } from './pages/reviews/reviews';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'chi-siamo',
+    component: About
+  },
+  {
+    path: 'servizi',
+    component: Services
+  },
+  {
+    path: 'galleria',
+    component: Gallery
+  },
+  {
+    path: 'recensioni',
+    component: Reviews
+  },
+  {
+    path: 'contatti',
+    component: Contact
+  }
+];
